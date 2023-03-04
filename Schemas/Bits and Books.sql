6663e09cc9185fc102b3dd9bcbe4043f5a4062ee
Canaan Porter <porter.1131@osu.edu>
2023-03-03 19:46:50 -0500

    Implemented generators for the rest of the db

--- Schemas/Bits and Books.sql
+++ Schemas/Bits and Books.sql
@@ -13,7 +13,7 @@ CREATE TABLE PUBLISHER (
 );
 
 CREATE TABLE BOOK (
-    isbn INTEGER(13) NOT NULL PRIMARY KEY,
+    isbn VARCHAR(13) NOT NULL PRIMARY KEY,
     release_year INTEGER(4) NOT NULL,
     sales_price DECIMAL(10,2) NOT NULL,
     title VARCHAR(255) NOT NULL,
@@ -42,28 +42,30 @@ CREATE TABLE WRITTEN_BY (
 
 CREATE TABLE USER (
     user_id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     fname VARCHAR(50) NOT NULL,
     lname VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone_no INTEGER(10),
     address VARCHAR(100) NOT NULL,
+    secondary_address VARCHAR(50),
     city VARCHAR(30) NOT NULL,
     state VARCHAR(2) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
     country VARCHAR(3) NOT NULL
 );
 
-CREATE TABLE PAYMENT (
-    bill_no INTEGER NOT NULL PRIMARY KEY,
-    sales_date DATE NOT NULL
-);
+-- CREATE TABLE PAYMENT (
+--     bill_no INTEGER NOT NULL PRIMARY KEY,
+--     sale_date DATE NOT NULL
+-- );
 
 CREATE TABLE "ORDER" (
     order_id INTEGER NOT NULL PRIMARY KEY,
-    sales_date DATE NOT NULL,
+    sale_date DATE NOT NULL,
     bill_no INTEGER NOT NULL,
     customer_id INTEGER NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES USER (user_id),
-    FOREIGN KEY (bill_no) REFERENCES PAYMENT (bill_no)
+    FOREIGN KEY (customer_id) REFERENCES USER (user_id)
 );
 
 CREATE TABLE WAREHOUSE (
@@ -71,6 +73,7 @@ CREATE TABLE WAREHOUSE (
     address VARCHAR(100) NOT NULL,
     city VARCHAR(30) NOT NULL,
     state VARCHAR(2) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
     country VARCHAR(3) NOT NULL,
     total_capacity INTEGER NOT NULL
 );
@@ -93,17 +96,17 @@ CREATE TABLE WAREHOUSE_STOCK (
     FOREIGN KEY (warehouse_id) REFERENCES WAREHOUSE (warehouse_id)
 );
 
-CREATE TABLE ACCESS_LEVEL (
-    level INTEGER NOT NULL PRIMARY KEY,
-    permissions VARCHAR(3) NOT NULL
-);
+-- CREATE TABLE ACCESS_LEVEL (
+--     level INTEGER NOT NULL PRIMARY KEY,
+--     permissions VARCHAR(3) NOT NULL
+-- );
 
 CREATE TABLE EMPLOYEE (
     employee_id INTEGER NOT NULL,
-    access INTEGER NOT NULL,
+    -- access INTEGER NOT NULL,
     warehouse_id INTEGER,
     position VARCHAR(25) NOT NULL,
     FOREIGN KEY (employee_id) REFERENCES USER(user_id),
-    FOREIGN KEY (warehouse_id) REFERENCES WAREHOUSE(warehouse_id),
-    FOREIGN KEY (access) REFERENCES ACCESS_LEVEL (level)
+    FOREIGN KEY (warehouse_id) REFERENCES WAREHOUSE(warehouse_id)
+    -- FOREIGN KEY (access) REFERENCES ACCESS_LEVEL (level)
 );
