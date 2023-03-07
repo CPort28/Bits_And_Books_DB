7fd9948969399a252befbd7d1fee4f4945f9b5af
Canaan Porter <porter.1131@osu.edu>
2023-03-07 10:05:22 -0500

    Added new relational diagram

---
+++ Model/book.rb
@@ -0,0 +1,22 @@
+###
+# Class : Book
+#
+# Used to generate store the B & B database definition of a book
+# 
+#
+
+class Book
+  ##
+  # Creates a new book object
+  def initialize(isbn, release_year, sales_price, title, pub_id, cat_id)
+    @isbn = isbn
+    @release_year = release_year
+    @sales_price = sales_price
+    @title = title
+    @pub_id = pub_id
+    @cat_id = cat_id
+    @popularity_rating = rand(1..100)
+  end
+
+
+end
